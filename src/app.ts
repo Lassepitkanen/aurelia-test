fc5e64c85a22c0afd72b43acda7a29cbd463a261
lassepitknen <lassepitknen@gmail.com>
2018-02-20 22:49:22 +0200

    test11

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,14 @@
+import {RouterConfiguration, Router} from 'aurelia-router';
+
 export class App {
-  message = 'Hello World!';
+  router: Router;
+
+  configureRouter(config: RouterConfiguration, router: Router): void {
+    this.router = router;
+    config.title = 'Aurelia';
+
+    config.map([
+      { route: ['', 'home'],  name: 'home',  moduleId: 'components/home/home', nav: true, title: 'Home' }
+    ]);
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -25,7 +25,7 @@ export function configure(aurelia: Aurelia) {
   }
 
   if (environment.testing) {
-    aurelia.use.plugin(PLATFORM.moduleName('aurelia-testing'));
+    aurelia.use.plugin(PLATFORM.moduleName('aurelia-validation'))
   }
 
   aurelia.start().then(() => aurelia.setRoot(PLATFORM.moduleName('app')));
