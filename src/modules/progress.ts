d912b25904229afde9169b249439df100f6bfb0d
lassepitknen <lassepitknen@gmail.com>
2018-03-04 16:52:23 +0200

    feat(progress)add button inc-/decredement functionality

--- src/modules/progress.ts
+++ src/modules/progress.ts
@@ -1,1 +1,28 @@
-export class Progress {}
+import { bindable} from "aurelia-templating";
+
+export class Progress {
+  @bindable()
+  public progress: number;
+  public incrementBy: number;
+  public decrementBy: number;
+
+  constructor(){
+    this.progress = 0;
+    this.incrementBy = 10;
+    this.decrementBy = 25;
+  }
+
+  public incrementProgress(){
+    if(this.progress + this.incrementBy >= 100){
+      this.progress = 100;
+    }
+    else (this.progress += this.incrementBy);
+  }
+
+  public decrementProgress(){
+    if(this.progress - this.decrementBy <= 0){
+      this.progress = 0;
+    }
+    else (this.progress -= this.decrementBy);
+  }
+}
