df79d0cefd2a28a7c4a868a0c1757bfe7e2c105f
lassepitknen <lassepitknen@gmail.com>
2018-03-04 16:34:48 +0200

    fix(app, accordion)fix typo & delete unnecessary binds

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,7 @@ export class App {
       { route: 'nag',  name: 'nag', moduleId: PLATFORM.moduleName('modules/nag'),  nav: true, title:'Nag' },
       { route: 'popup',  name: 'popup', moduleId: PLATFORM.moduleName('modules/popup'),  nav: true, title:'Popup' },
       { route: 'progress',  name: 'progress', moduleId: PLATFORM.moduleName('modules/progress'),  nav: true, title:'Progress' },
-      { route: 'rating',  name: 'rating', moduleId: PLATFORM.moduleName('modules/rating'),  nav: true, title:'Pating' },
+      { route: 'rating',  name: 'rating', moduleId: PLATFORM.moduleName('modules/rating'),  nav: true, title:'Rating' },
       { route: 'search',  name: 'search', moduleId: PLATFORM.moduleName('modules/search'),  nav: true, title:'Search' },
       { route: 'shape',  name: 'shape', moduleId: PLATFORM.moduleName('modules/shape'),  nav: true, title:'Shape' },
       { route: 'sidebar',  name: 'sidebar', moduleId: PLATFORM.moduleName('modules/sidebar'),  nav: true, title:'Sidebar' },
